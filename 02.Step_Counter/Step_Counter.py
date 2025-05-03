db5bdb9b1d7181625b8f67df807b5c4a278a809f
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2025-05-04 00:04:15 +0600

    Step Counter project done

---
+++ 02.Step_Counter/Step_Counter.py
@@ -0,0 +1,9 @@
+print("Step Counter -")
+daily_goal = int(input("What is your daily step goal?\n"))
+current_steps = int(input("How may steps you have taken today?\n"))
+
+remaining = daily_goal - current_steps
+if remaining > 0:
+  print(f"You need {remaining} more steps to reach your goal!")
+else:
+  print(f"Congratulaion! You've exceeded your goal by {-remaining} steps!")
