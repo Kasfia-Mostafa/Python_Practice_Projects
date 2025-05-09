9d94df57e2c093a27429dea79972f96f6176be0a
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2025-05-09 14:02:21 +0600

    Character Type Checker.

---
+++ Character_Type_Checker.py
@@ -0,0 +1,10 @@
+print("Character Type Checker")
+
+char = input("Enter a single character: ")
+
+if char.isalpha():
+  print("This a letter.")
+elif char.isdigit():
+  print("This is a digit")
+else:
+  print("This is a special character")
