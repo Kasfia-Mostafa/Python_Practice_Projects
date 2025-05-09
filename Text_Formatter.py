fed367d0d1ac475d77cc1d6b74509f1ada2a3cc3
Kasfia-Mostafa <kasfiamostafa03@gmail.com>
2025-05-09 13:52:03 +0600

    Text Formatter

---
+++ Text_Formatter.py
@@ -0,0 +1,17 @@
+print("TEXT CAPITALIZER")
+text = input("Enter some text: ")
+print("1.Uppercase")
+print("2.Lowercase")
+print("3.Title case")
+print("4.Sentence case")
+
+choice = input("Choose a format (1-4):")
+
+if choice == '1':
+  print(text.upper())
+elif choice == '2':
+  print(text.lower())
+elif choice == '3':
+  print(text.title())
+elif choice == '4':
+  print(text.capitalize())
